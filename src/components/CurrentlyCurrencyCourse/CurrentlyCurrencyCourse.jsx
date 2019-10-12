e6f1c2abe087ae392eae127aefb36d6dca34c232
AntonGoncharik <ant.goncharik@gmail.com>
2019-10-12 20:13:35 +0300

    received all currency and receive course

--- src/components/CurrentlyCurrencyCourse/CurrentlyCurrencyCourse.jsx
+++ src/components/CurrentlyCurrencyCourse/CurrentlyCurrencyCourse.jsx
@@ -6,44 +6,44 @@ import {connect} from "react-redux";
 import {getCurrencies, getCurrencyCourse} from "../../redux/currencyCourseReducer";
 
 const CurrentlyCurrencyCourse = (props) => {
-    const [openedList, openCloseList] = useState(false);
+        const [openedList, openCloseList] = useState(false);
 
-    useEffect(() => {
-        props.getCurrencies();
-    }, [props.currencies.length]);
+        useEffect(() => {
+            props.getCurrencies();
+            props.getCurrencyCourse(145);
+        }, [props.currencies.length]);
 
-    const course = props.currenciesCourses.find(item =>
-        item.currencyId === props.selectedCurrencyId).course;
+        const course = props.currenciesCourses.find(item => item.currencyId === +props.selectedCurrencyId).course;
 
-    const currencyAbbreviation = props.currencies.find(item =>
-        item.currencyId === props.selectedCurrencyId).currencyAbbreviation;
+        const currencyAbbreviation = props.currencies.find(item => item.currencyId === +props.selectedCurrencyId).currencyAbbreviation;
 
-    const changeCurrency = (e) => {
-        openCloseList(false);
-        props.getCurrencyCourse(e.currentTarget.value);
-    }
+        const changeCurrency = (e) => {
+            openCloseList(false);
+            props.getCurrencyCourse(e.currentTarget.value);
+        };
 
-    return (
-        <div>
-            <div className={cn(s.blockCourse, s.blockText)}>
-                <div>CURRENCY</div>
-                <div>COURSE</div>
-                {openedList &&
-                <select defaultValue={props.selectedCurrencyId}
-                        className={cs.select}
-                        onChange={(e) => changeCurrency(e)}
-                        onBlur={(e) => changeCurrency(e)}>
-                    {props.currencies.map(item =>
-                        <option key={item.currencyId}
-                                value={item.currencyId}>{item.currencyAbbreviation}</option>)}
-                </select> ||
-                <div className={s.blockCurrentlyCourse}
-                     onClick={() => openCloseList(true)}>{currencyAbbreviation}</div>}
-                <div className={s.blockCurrentlyCourse}>{course}</div>
+        return (
+            <div>
+                <div className={cn(s.blockCourse, s.blockText)}>
+                    <div>CURRENCY</div>
+                    <div>COURSE</div>
+                    {openedList &&
+                    <select defaultValue={props.selectedCurrencyId}
+                            className={cs.select}
+                            onChange={(e) => changeCurrency(e)}
+                            onBlur={(e) => changeCurrency(e)}>
+                        {props.currencies.map(item =>
+                            <option key={item.currencyId}
+                                    value={item.currencyId}>{item.currencyAbbreviation}</option>)}
+                    </select> ||
+                    <div className={s.blockCurrentlyCourse}
+                         onClick={() => openCloseList(true)}>{currencyAbbreviation}</div>}
+                    <div className={s.blockCurrentlyCourse}>{course}</div>
+                </div>
             </div>
-        </div>
-    )
-};
+        )
+    }
+;
 
 const mapStateToProps = (state) => {
     return {
--- src/redux/currencyCourseReducer.js
+++ src/redux/currencyCourseReducer.js
@@ -3,11 +3,12 @@ import {getCurrenciesAPI, getCurrencyCourseAPI} from "../API/officialCurrenciesC
 const GET_CURRENCIES = 'GET_CURRENCIES';
 const GET_CURRENCY_COURSE = 'GET_CURRENCY_COURSE';
 const GET_CURRENCY_COURSE_TO_PERIOD = 'GET_CURRENCY_COURSE_TO_PERIOD';
+const SET_SELECTED_CURRENCY = 'SET_SELECTED_CURRENCY';
 
 const initialState = {
-    currencies: [{currencyId: 145, currencyAbbreviation: 'USD', currencyName: 'US Dollar'}],
-    currenciesCourses: [{currencyId: 145, scale: 1, course: 2, date: ''}],
-    currencyCourseToPeriod: [{currencyId: 145, course: 2, date: ''}],
+    currencies: [{currencyId: 145, currencyAbbreviation: '', currencyName: ''}],
+    currenciesCourses: [{currencyId: 145, scale: 0, course: 0, date: ''}],
+    currencyCourseToPeriod: [{currencyId: 145, course: 0, date: ''}],
     selectedCurrencyId: 145
 };
 
@@ -26,16 +27,23 @@ export const currencyCourseReducer = (state = initialState, action) => {
         case GET_CURRENCY_COURSE:
             return {
                 ...state, currenciesCourses: [...state.currenciesCourses,
-                    state.currenciesCourses.find(item => {item.currencyId === })
-                    ({
+                    {
                         currencyId: action.dataCurrency.Cur_ID,
                         scale: action.dataCurrency.Cur_Scale,
                         course: action.dataCurrency.Cur_OfficialRate,
                         date: action.dataCurrency.Date
-                    })]
+                    }
+                ]
             };
             break;
-        case GET_CURRENCY_COURSE_TO_PERIOD:
+        case
+        GET_CURRENCY_COURSE_TO_PERIOD:
+            break;
+        case
+        SET_SELECTED_CURRENCY:
+            return {
+                ...state, selectedCurrencyId: action.id
+            };
             break;
         default:
             return state;
@@ -54,12 +62,17 @@ const getCurrencyCourseToPeriodAC = () => {
     return {type: 'GET_CURRENCY_COURSE_TO_PERIOD'}
 };
 
+const setSelectedCurrencyAC = (id) => {
+    return {type: 'SET_SELECTED_CURRENCY', id}
+};
+
 export const getCurrencies = () => {
     return async (dispatch) => {
         try {
             const result = await getCurrenciesAPI();
             if (result.status === 200) {
-                dispatch(getCurrenciesAC(result.data));
+                const currencies = result.data.filter(item => new Date(item.Cur_DateEnd) > new Date());
+                dispatch(getCurrenciesAC(currencies));
             }
         } catch (e) {
 
@@ -68,11 +81,16 @@ export const getCurrencies = () => {
 }
 
 export const getCurrencyCourse = (id) => {
-    return async (dispatch) => {
+    return async (dispatch, getState) => {
         try {
             const result = await getCurrencyCourseAPI(id);
             if (result.status === 200) {
-                dispatch(getCurrencyCourseAC(result.data));
+                const currencies = getState().currenciesCourses.currenciesCourses;
+                const hasCurrency = currencies.find(item => item.currencyId === +id);
+                if (!hasCurrency) {
+                    dispatch(getCurrencyCourseAC(result.data));
+                }
+                dispatch(setSelectedCurrencyAC(id));
             }
         } catch (e) {
 
