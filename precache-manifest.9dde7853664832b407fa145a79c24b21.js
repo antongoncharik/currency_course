98957da148c831a243421101f504d020eddf4c08
AntonGoncharik <ant.goncharik@gmail.com>
2019-10-15 23:16:01 +0300

    Updates

---
+++ precache-manifest.9dde7853664832b407fa145a79c24b21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52892043e04d94abb861b84b8315ea31",
+    "url": "/currency_course/index.html"
+  },
+  {
+    "revision": "049d84bd15f23afd6122",
+    "url": "/currency_course/static/css/2.c6231af7.chunk.css"
+  },
+  {
+    "revision": "59a04579bf5fdd4c67f9",
+    "url": "/currency_course/static/css/main.fe14daac.chunk.css"
+  },
+  {
+    "revision": "049d84bd15f23afd6122",
+    "url": "/currency_course/static/js/2.5e202a5f.chunk.js"
+  },
+  {
+    "revision": "59a04579bf5fdd4c67f9",
+    "url": "/currency_course/static/js/main.e3f02373.chunk.js"
+  },
+  {
+    "revision": "6658949c79ce127a6e8e",
+    "url": "/currency_course/static/js/runtime-main.18cabf29.js"
+  }
+]);
